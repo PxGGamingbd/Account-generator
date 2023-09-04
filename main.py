d1ce62fd83b677c60488667773f651f0b3683c4b
PxGGamingbd <99589521+PxGGamingbd@users.noreply.github.com>
2023-09-04 19:20:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import os
+import time
+import json
+from datetime import datetime
+from random import randint
+from threading import Thread
+from base64 import b64encode
+from hashlib import sha1
+from hmac import new
+
+os.system('pip install -r requirements.txt')
+
+import os
 from k_amino import(
 Client,
 SubClient
